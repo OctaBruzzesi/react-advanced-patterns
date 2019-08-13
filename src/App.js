5c7f89f215ad4594a272ef70b610374c7dbc256b
Octa Bruzzesi <octa.bruzzesi>
2019-08-13 13:46:30 -0300

    ADD: redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import logo from './logo.svg';
 import './App.css';
+import reducer from './redux/reducer';
+
+const store = createStore(reducer);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <main>
+          <p>Demo</p>
+        </main>
+      </div>
+    </Provider>
   );
 }
 
